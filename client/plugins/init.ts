4c63ede48ca88de6e734c465ced53cf2b8963c6e
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2024-05-02 23:31:50 +0300

    upd

--- client/plugins/init.ts
+++ client/plugins/init.ts
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.hook('app:beforeMount', async () => {
-      const socket = io('http://localhost:3000/');
+      const socket = io('/');
       socket.on('connect', function() {
         console.log('Connected');
       });
